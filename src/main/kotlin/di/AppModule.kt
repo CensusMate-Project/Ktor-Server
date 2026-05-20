648aba4cbf669c688fc39c6dc2e110dc7a0b878b
Maxim Plotnikov <develop.kisa@gmail.com>
2026-05-20 12:02:03 +0300

    feat: add use cases for managing persons with create, update, delete, and retrieval functionalities

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -11,9 +11,11 @@ import org.censusmate.controller.UserController
 import org.censusmate.data.remote.dadata.DaDataClient
 import org.censusmate.data.repository.EventRepositoryImpl
 import org.censusmate.data.repository.HouseholdRepositoryImpl
+import org.censusmate.data.repository.PersonRepositoryImpl
 import org.censusmate.data.repository.UserRepositoryImpl
 import org.censusmate.domain.repository.EventRepository
 import org.censusmate.domain.repository.HouseholdRepository
+import org.censusmate.domain.repository.PersonRepository
 import org.censusmate.domain.repository.UserRepository
 import org.censusmate.domain.usecase.auth.GetMeUseCase
 import org.censusmate.domain.usecase.auth.LoginUseCase
@@ -28,6 +30,11 @@ import org.censusmate.domain.usecase.household.DeleteHouseholdUseCase
 import org.censusmate.domain.usecase.household.GetHouseholdUseCase
 import org.censusmate.domain.usecase.household.GetHouseholdsUseCase
 import org.censusmate.domain.usecase.household.UpdateHouseholdUseCase
+import org.censusmate.domain.usecase.persons.CreatePersonUseCase
+import org.censusmate.domain.usecase.persons.DeletePersonUseCase
+import org.censusmate.domain.usecase.persons.GetPersonUseCase
+import org.censusmate.domain.usecase.persons.GetPersonsUseCase
+import org.censusmate.domain.usecase.persons.UpdatePersonUseCase
 import org.censusmate.domain.usecase.suggest.SuggestAddressUseCase
 import org.censusmate.domain.usecase.user.BlockUserUseCase
 import org.censusmate.domain.usecase.user.CreateUserUseCase
@@ -48,6 +55,7 @@ object AppContainer {
     val userRepository: UserRepository by lazy { UserRepositoryImpl() }
     val eventRepository: EventRepository by lazy { EventRepositoryImpl() }
     val householdRepository: HouseholdRepository by lazy { HouseholdRepositoryImpl() }
+    val personRepository: PersonRepository by lazy { PersonRepositoryImpl() }
 
     val loginUseCase: LoginUseCase by lazy { LoginUseCase(userRepository) }
     val getMeUseCase: GetMeUseCase by lazy { GetMeUseCase(userRepository) }
@@ -82,6 +90,12 @@ object AppContainer {
     }
     val deleteHouseholdUseCase: DeleteHouseholdUseCase by lazy { DeleteHouseholdUseCase(householdRepository) }
 
+    val getPersonsUseCase by lazy { GetPersonsUseCase(personRepository) }
+    val getPersonUseCase by lazy { GetPersonUseCase(personRepository) }
+    val createPersonUseCase by lazy { CreatePersonUseCase(personRepository) }
+    val updatePersonUseCase by lazy { UpdatePersonUseCase(personRepository, householdRepository) }
+    val deletePersonUseCase by lazy { DeletePersonUseCase(personRepository, householdRepository) }
+
     val authController: AuthController by lazy { AuthController(loginUseCase, getMeUseCase) }
     val userController: UserController by lazy {
         UserController(getUsersUseCase, getUserUseCase, createUserUseCase, updateUserUseCase, blockUserUseCase)
@@ -106,6 +120,7 @@ object AppContainer {
             deleteHouseholdUseCase
         )
     }
+
 
     fun init(config: Config) {
         this.config = config
